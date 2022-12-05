0d69526a8ebf51382bf4e69218fafd56f80c8b20
anof1r <denchik.anofriev@gmail.com>
2022-12-05 17:05:11 +0300

    SideMenu component

--- src/pages/ThemeOne.jsx
+++ src/pages/ThemeOne.jsx
@@ -3,6 +3,7 @@ import Champions from '../components/Champions/Champions'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import Hero from '../components/Hero/Hero'
+import ModalMenu from '../components/ModalMenu/ModalMenu'
 import Work from '../components/Work/Work'
 
 function ThemeOne() {
@@ -13,6 +14,7 @@ function ThemeOne() {
         <Work />
         <Champions />
         <Footer />
+        <ModalMenu />
     </div>
   )
 }
